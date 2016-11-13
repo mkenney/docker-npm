f4ef8fcd5330f78639009326a712cb4a1e6e6d35
Michael Kenney <michael.kenney@returnpath.net>
2016-11-13 13:44:06 -0700

    travis-ci testing

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -3,20 +3,11 @@
 
 
 # Test execution method
+source $(dirname `pwd`)/test/lib/travis.sh
 source $(dirname `pwd`)/test/lib/assert.sh
 
 # List of tests to execute
 declare -a TESTS=()
-
-#
-# Get the current and parent branch names
-# based on https://gist.github.com/intel352/9761288#gistcomment-1774649
-#
-export CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-vbc_col=$(( $(git show-branch | grep '^[^\[]*\*' | head -1 | cut -d* -f1 | wc -c) - 1 ))
-swimming_lane_start_row=$(( $(git show-branch | grep -n "^[\-]*$" | cut -d: -f1) + 1 ))
-export PARENT_BRANCH=`git show-branch | tail -n +$swimming_lane_start_row | grep -v "^[^\[]*\[$CURRENT_BRANCH" | grep "^.\{$vbc_col\}[^ ]" | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//'`
-if [ "" == "$PARENT_BRANCH" ]; then PARENT_BRANCH=master; fi
 
 echo "
 travis branch: $TRAVIS_BRANCH
