5e3182bd147898be574a5f262bd2b37741e769f1
Michael Kenney <michael.kenney@returnpath.net>
2016-11-13 13:14:35 -0700

    cleanup tests

--- test/build.sh
+++ test/build.sh
@@ -3,21 +3,19 @@
 PREFIX="        "
 TAG=ci-build
 project_path=$(dirname `pwd`)
+build_result=0
+failed_tests=
 
 cd $project_path
 docker build -t mkenney/npm:$TAG .
 result=$?
 echo $output
 if [ 0 -ne $result ]; then
-    echo "${PREFIX}command failed: 'docker build'"
+    build_result=1
+    failed_tests="$failed_tests docker-build"
 fi
 
 cd $project_path/test
-build_result=0
-
-docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$TAG /run-as-user /usr/local/bin/node --version
-
-failed_tests=
 output=$(docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$TAG /run-as-user /usr/local/bin/node        --version); result=$?; echo $output; if [ 0 -ne $result ]; then build_result=1; failed_tests="$failed_tests node";  fi;
 output=$(docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$TAG /run-as-user /usr/local/bin/bower       --version); result=$?; echo $output; if [ 0 -ne $result ]; then build_result=1; failed_tests="$failed_tests bower"; fi;
 output=$(docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$TAG /run-as-user /usr/local/bin/npm         --version); result=$?; echo $output; if [ 0 -ne $result ]; then build_result=1; failed_tests="$failed_tests npm";   fi;
@@ -26,5 +24,7 @@ output=$(docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$TAG /run-as-user /us
 output=$(docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$TAG /run-as-user /usr/local/bin/grunt       --version); result=$?; echo $output; if [ 0 -ne $result ]; then build_result=1; failed_tests="$failed_tests grunt"; fi;
 output=$(docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$TAG /run-as-user /usr/local/bin/gulp        --version); result=$?; echo $output; if [ 0 -ne $result ]; then build_result=1; failed_tests="$failed_tests gulp";  fi;
 
-echo "Failed tests: $failed_tests"
+if [ 0 -ne $build_result ]; then
+    echo "${PREFIX}Build tests failed: $failed_tests"
+fi
 exit $build_result
