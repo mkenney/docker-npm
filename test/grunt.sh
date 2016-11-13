1da850f1ba452ad4910743677833add6b336ba62
Michael Kenney <michael.kenney@returnpath.net>
2016-11-13 15:43:15 -0700

    travis-ci testing

--- test/grunt.sh
+++ test/grunt.sh
@@ -3,12 +3,14 @@
 PREFIX="        "
 
 CMD="$PROJECT_PATH/bin/grunt"
+NPM="$PROJECT_PATH/bin/npm"
 if [ "" != "$1" ]; then
     CMD="docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/grunt"
+    NPM="docker run --rm -ti -v $(pwd):/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/npm"
 fi
 
 cd $PROJECT_PATH/test/resources
-rm -rf node_modules
+rm -rf node_modules && $NPM install
 
 output=$($CMD)
 result=$?
