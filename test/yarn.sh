78f28c90a41d7d7edbe59f98ca96aa97e983a052
Michael Kenney <michael.kenney@returnpath.net>
2016-11-13 17:22:05 -0700

    travis-ci testing

--- test/yarn.sh
+++ test/yarn.sh
@@ -6,7 +6,6 @@ CMD="$PROJECT_PATH/bin/yarn"
 if [ "" != "$1" ]; then
     CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/yarn"
 fi
-echo "CMD: $CMD"
 
 cd $PROJECT_PATH/test/resources
 rm -rf node_modules
