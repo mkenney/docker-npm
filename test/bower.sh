298ad7ee9af987513aa01886f2e278e26d75311f
Michael Kenney <michael.kenney@returnpath.net>
2017-04-12 17:47:49 -0600

    Add update command to test scripts

--- test/bower.sh
+++ test/bower.sh
@@ -6,7 +6,7 @@ CMD="$PROJECT_PATH/bin/bower"
 if [ "" != "$1" ]; then
     CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower"
 else
-    $CMD self-update
+    eval "$CMD self-update"
 fi
 
 cd $PROJECT_PATH/test/resources
