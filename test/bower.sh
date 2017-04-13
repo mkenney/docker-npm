8bad17c62a2449abc539ab1fa9287ac63d9930b9
Michael Kenney <michael.kenney@returnpath.net>
2017-04-12 22:40:29 -0600

    test tests

--- test/bower.sh
+++ test/bower.sh
@@ -5,8 +5,6 @@ CMD="$PROJECT_PATH/bin/bower"
 
 if [ -n "$1" ]; then
     CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower"
-else
-    $CMD self-update
 fi
 
 echo "\$PROJECT_PATH: $PROJECT_PATH"
