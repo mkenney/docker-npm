6703020cc05b4662607e0ccbe12552a588476cc5
Michael Kenney <michael.kenney@returnpath.net>
2017-04-12 22:47:54 -0600

    test tests

--- test/bower.sh
+++ test/bower.sh
@@ -3,6 +3,7 @@
 PREFIX="        "
 CMD="$PROJECT_PATH/bin/bower"
 
+ls -laF /bin/bash
 if [ "" != "$1" ]
 then
     CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower"
