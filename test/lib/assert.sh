d08dfc1cd6e1d127c00a15a46269f74bf343059a
Michael Kenney <michael.kenney@returnpath.net>
2017-04-12 21:35:13 -0600

    test tests

--- test/lib/assert.sh
+++ test/lib/assert.sh
@@ -6,15 +6,19 @@ echo "start assert"
     script=$1
 
     expected=0
+echo "pre-error"
     if [ -z $2 ]; then
         expected=$2
     fi
+echo "post-error"
+echo "script: $(dirname `pwd`)/test/$script"
 
     #
     bash $(dirname `pwd`)/test/$script;
     result=$?
+echo "result: $result"
     echo "        expected: $expected, result: $result"
-    exit $result
 
 echo "end assert"
+    exit $result
 }
