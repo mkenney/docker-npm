1e682d6aa53b0d75d1fd67c22eac7b31bf3fec71
Michael Kenney <michael.kenney@returnpath.net>
2017-04-12 22:18:40 -0600

    test tests

--- test/lib/assert.sh
+++ test/lib/assert.sh
@@ -8,10 +8,10 @@ function assert {
     if [ "" != "$2" ]; then
         expected=$2
     fi
-exit 1
 
     #
     bash $(dirname `pwd`)/test/$script;
+exit 1
     result=$?
     echo "        expected: $expected, result: $result"
     exit $result
