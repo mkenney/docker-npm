892f8614352ea332e8bc4e9cc9613435c64bd2ed
Michael Kenney <michael.kenney@returnpath.net>
2017-04-12 22:27:38 -0600

    test tests

--- test/bower.sh
+++ test/bower.sh
@@ -2,6 +2,8 @@
 
 PREFIX="        "
 
+echo "got here"
+exit 1
 CMD="$PROJECT_PATH/bin/bower"
 if [ "" != "$1" ]; then
     CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower"
--- test/lib/assert.sh
+++ test/lib/assert.sh
@@ -4,15 +4,12 @@ function assert {
     script=$1
 
     expected=0
-
     if [ "" != "$2" ]; then
         expected=$2
     fi
 
     #
-echo bash $(dirname `pwd`)/test/$script
-exit 1
-    bash $(dirname `pwd`)/test/$script
+    bash $(dirname `pwd`)/test/$script;
     result=$?
     echo "        expected: $expected, result: $result"
     exit $result
