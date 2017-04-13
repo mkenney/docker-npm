2f0b9a000ca19e0681635c579af5ac703e0343e8
Michael Kenney <michael.kenney@returnpath.net>
2017-04-12 22:54:29 -0600

    test tests

--- test/bower.sh
+++ test/bower.sh
@@ -3,10 +3,10 @@
 PREFIX="        "
 CMD="$PROJECT_PATH/bin/bower"
 
-if [[ "" != "$1" ]]; then
-    CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower"
-else
+if [ "" = "$1" ]; then
     $CMD self-update
+else
+    CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$1 /run-as-user /usr/local/bin/bower"
 fi
 
 echo "\$PROJECT_PATH: $PROJECT_PATH"
