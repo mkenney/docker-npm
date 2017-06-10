b3f829f333a7a6efe7e61222b00cdf87a4b2c907
Michael Kenney <mkenney@webbedlam.com>
2017-06-09 22:36:12 -0600

    test travis

--- test/1.build.sh
+++ test/1.build.sh
@@ -13,6 +13,7 @@ if [ 0 -ne $result ]; then
 fi
 cd $PROJECT_PATH/test
 
+echo "executing node.sh"
 output=$(bash ./node.sh)
 result=$?
 echo $output
@@ -21,6 +22,7 @@ if [ 0 -ne $result ]; then
     failed_tests="$failed_tests node"
 fi;
 
+echo "executing bower.sh"
 output=$(bash ./bower.sh)
 result=$?
 echo $output
@@ -29,6 +31,7 @@ if [ 0 -ne $result ]; then
     failed_tests="$failed_tests bower";
 fi;
 
+echo "executing npm.sh"
 output=$(bash ./npm.sh)
 result=$?
 echo $output
@@ -37,6 +40,7 @@ if [ 0 -ne $result ]; then
     failed_tests="$failed_tests npm";
 fi;
 
+echo "executing yarn.sh"
 output=$(bash ./yarn.sh)
 result=$?
 echo $output
@@ -45,6 +49,7 @@ if [ 0 -ne $result ]; then
     failed_tests="$failed_tests yarn"
 fi;
 
+echo "executing md.sh"
 output=$(bash ./md.sh)
 result=$?
 echo $output
@@ -53,6 +58,7 @@ if [ 0 -ne $result ]; then
     failed_tests="$failed_tests md";
 fi;
 
+echo "executing grunt.sh"
 output=$(bash ./grunt.sh)
 result=$?
 echo $output
@@ -61,6 +67,7 @@ if [ 0 -ne $result ]; then
     failed_tests="$failed_tests grunt";
 fi;
 
+echo "executing gulp.sh"
 output=$(bash ./gulp.sh)
 result=$?
 echo $output
@@ -68,7 +75,6 @@ if [ 0 -ne $result ]; then
     build_result=1
     failed_tests="$failed_tests gulp"
 fi;
-
 
 if [ 0 -ne $build_result ]; then
     echo "${PREFIX}build.sh tests failed: $failed_tests"
