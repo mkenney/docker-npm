ac2ab56c3743169c46795c28bf7bbb0d0d8c8f5c
Michael Kenney <michael.kenney@returnpath.com>
2018-05-20 14:22:46 -0600

    test cleanup

--- test/bower.sh
+++ test/bower.sh
@@ -6,18 +6,16 @@ if [ "" != "$1" ]; then
     IMAGE_TAG=$1
 fi
 
-CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF"
+CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /usr/local/bin/bower  --allow-root"
 
 cd $PROJECT_PATH/test/resources
 rm -rf bower_components
 
-output=`$CMD`
+output=`$CMD install`
 result=$?
-if [ 0 -ne $result ]; then
     echo "${PREFIX}command failed: '$CMD install'"
     echo "${PREFIX}${PREFIX}${output}"
     exit $result
-fi
 
 output=`ls bower_components`
 result=$?
