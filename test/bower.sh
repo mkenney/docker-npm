9b4aae6cf67e0459427eb2715360d3035ce8803b
Michael Kenney <mkenney@webbedlam.com>
2018-07-31 21:34:35 -0600

    build test

--- test/bower.sh
+++ test/bower.sh
@@ -12,10 +12,19 @@ cd $PROJECT_PATH/test/resources
 
 #build testing
 
+echo "ENV:"
 echo "User $(whoami): $(id -u dev):$(id -g dev)"
-
-echo "stats: $(stat -c '%u' /src):$(stat -c '%g' /src)"
+echo "stats: $(stat -c '%u' `pwd`):$(stat -c '%g' `pwd`)"
 ls -laF
+echo
+echo "CONTAINER:"
+echo "whoami: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG whoami)"
+echo "uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -u dev)"
+echo "gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -g dev)"
+
+echo "/src uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%u' /src)"
+echo "/src gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%g' /src)"
+docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF
 exit 1
 
 rm -rf bower_components
