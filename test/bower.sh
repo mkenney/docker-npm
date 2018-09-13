a56f121de60d0f56cbeddd51cabbd20fecb18d0c
Michael Kenney <mkenney@webbedlam.com>
2018-09-12 21:00:48 -0600

    upgrade yarn

--- test/bower.sh
+++ test/bower.sh
@@ -18,13 +18,13 @@ echo "stats: $(stat -c '%u' `pwd`):$(stat -c '%g' `pwd`)"
 ls -laF
 echo
 echo "CONTAINER:"
-echo "whoami: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /run-as-user whoami)"
-echo "uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /run-as-user id -u dev)"
-echo "gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /run-as-user id -g dev)"
+echo "whoami: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG whoami)"
+echo "uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -u dev)"
+echo "gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -g dev)"
 
-echo "/src uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /run-as-user stat -c '%u' /src)"
-echo "/src gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /run-as-user stat -c '%g' /src)"
-docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /run-as-user ls -laF
+echo "/src uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%u' /src)"
+echo "/src gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%g' /src)"
+docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF
 exit 1
 
 rm -rf bower_components
