148b70674e8364d0674a0ab6f322619d3a2aebcb
Michael Kenney <mkenney@webbedlam.com>
2018-09-12 21:09:03 -0600

    test build

--- test/bower.sh
+++ test/bower.sh
@@ -18,13 +18,13 @@ echo "stats: $(stat -c '%u' `pwd`):$(stat -c '%g' `pwd`)"
 ls -laF
 echo
 echo "CONTAINER:"
-echo "whoami: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG whoami)"
-echo "uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -u dev)"
-echo "gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -g dev)"
+echo "whoami: $(docker run --rm -ti -e PUID=$(id -u $(whoami)) -e PGID=$(id -g $(whoami)) -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG whoami)"
+echo "uid: $(docker run --rm -ti -e PUID=$(id -u $(whoami)) -e PGID=$(id -g $(whoami)) -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -u dev)"
+echo "gid: $(docker run --rm -ti -e PUID=$(id -u $(whoami)) -e PGID=$(id -g $(whoami)) -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG id -g dev)"
 
-echo "/src uid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%u' /src)"
-echo "/src gid: $(docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%g' /src)"
-docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF
+echo "/src uid: $(docker run --rm -e PUID=$(id -u $(whoami)) -e PGID=$(id -g $(whoami)) -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%u' /src)"
+echo "/src gid: $(docker run --rm -e PUID=$(id -u $(whoami)) -e PGID=$(id -g $(whoami)) -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG stat -c '%g' /src)"
+docker run --rm  -e PUID=$(id -u $(whoami)) -e PGID=$(id -g $(whoami)) -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF
 exit 1
 
 rm -rf bower_components
