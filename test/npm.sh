4e677f3bada97addb4ee752ffcb76a36740c0b45
Michael Kenney <mkenney@webbedlam.com>
2018-09-13 21:56:52 -0600

    test build

--- test/npm.sh
+++ test/npm.sh
@@ -6,37 +6,6 @@ if [ "" != "$1" ]; then
     IMAGE_TAG=$1
 fi
 
-CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG"
-echo "###########################################################################"
-echo "###########################################################################"
-echo "###########################################################################"
-echo
-echo "ls -laF /home/dev/.config/"
-$CMD ls -laF /home/dev/.config/
-echo
-echo "ls -laF /home/dev/.config/configstore/"
-$CMD ls -laF /home/dev/.config/configstore/
-echo
-echo "cat /run-as-user"
-$CMD cat /run-as-user
-echo
-echo "cat /etc/group"
-$CMD cat /etc/group
-echo
-echo "pwd"
-$CMD pwd
-echo
-echo "whoami"
-$CMD whoami
-echo
-echo "env"
-$CMD env
-echo
-echo "###########################################################################"
-echo "###########################################################################"
-echo "###########################################################################"
-exit 1
-
 CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /usr/local/bin/npm"
 
 cd $PROJECT_PATH/test/resources
