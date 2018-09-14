deda535b4ab5a17114c24fc644237655f8a6cbed
Michael Kenney <mkenney@webbedlam.com>
2018-09-13 20:49:29 -0600

    test build

--- test/bower.sh
+++ test/bower.sh
@@ -5,6 +5,19 @@ IMAGE_TAG=latest
 if [ "" != "$1" ]; then
     IMAGE_TAG=$1
 fi
+echo "###########################################################################"
+echo
+echo "docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF /home/dev/.config/"
+docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF /home/dev/.config/
+echo
+echo "docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF /home/dev/.config/configstore/"
+docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG ls -laF /home/dev/.config/configstore/
+echo
+echo "###########################################################################"
+exit 1
+
+
+
 
 CMD="docker run --rm -ti -v $PROJECT_PATH/test/resources:/src:rw mkenney/npm:$IMAGE_TAG /usr/local/bin/bower  --allow-root"
 
